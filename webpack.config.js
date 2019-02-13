c359c8f327340400de69d9ad8c76407fbbc2e434
swaggerloaf <swaggerloaf@gmail.com>
2019-02-13 09:58:11 -0500

    Updates

--- webpack.config.js
+++ webpack.config.js
@@ -9,38 +9,43 @@ const publicPath = '/';
 // Omit trailing slash as %PUBLIC_PATH%/xyz looks better than %PUBLIC_PATH%xyz.
 const publicUrl = '';
 
-
 module.exports = {
-   mode: "development",
-    // starting point
-    entry: "./app/index.js",
-    output: {
-        path: path.join(__dirname, "./dist"),
-        filename: "bundle.js",
-        //publicPath:'/dist'
-    },
-    module: {
-      rules: [
-        { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-        { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
-        {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader']
-        },
-        {
-        test   : /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
-        loader : 'file-loader'
-        }
-
-      ]
-    },
-    context: __dirname,
-    plugins: [
-    ],
-    devtool: "source-map",
-    devServer: {
+  mode: 'development',
+  // starting point
+  entry: './app/index.js',
+  output: {
+    path: path.join(__dirname, './dist'),
+    filename: 'bundle.js'
+    //publicPath:'/dist'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader', // creates style nodes from JS strings
+          'css-loader', // translates CSS into CommonJS
+          'sass-loader' // compiles Sass to CSS, using Node Sass by default
+        ]
+      },
+      {
+        test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+        loader: 'file-loader'
+      }
+    ]
+  },
+  context: __dirname,
+  plugins: [],
+  devtool: 'source-map',
+  devServer: {
     historyApiFallback: true,
     hot: false,
     port: 4040
   }
-}
+};
